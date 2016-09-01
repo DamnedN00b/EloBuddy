7f537e45bc2ddeb139e56147b809dc72b0fc42b3
DamnedNooB <singed1306@gmail.com>
2016-09-01 06:26:32 +0200

    v1.0.0.4

--- LazyLucian/LazyLucian/Modes/Combo.cs
+++ LazyLucian/LazyLucian/Modes/Combo.cs
@@ -26,20 +26,96 @@ namespace LazyLucian.Modes
                 if (!(Settings.SpellWeaving &&
                       (Program.Player.HasBuff("LucianPassiveBuff") || Program.Player.IsDashing() ||
                        Orbwalker.IsAutoAttacking || CustomEvents.PassiveUp)) &&
+                    !(Program.Player.Distance(enemy) <= SpellManager.Q.Range &&
+                      (Q.IsReady() && enemy.Distance(Program.Player) < Program.Player.GetAutoAttackRange())))
                 {
                     SpellManager.E.Cast((Vector3) Program.Player.Position.Extend(Game.CursorPos, SpellManager.E.Range));
                 }
 
+                if (SpellManager.Q.IsReady() && !Program.Player.Spellbook.IsCastingSpell &&
+                    !(Settings.SpellWeaving &&
+                      (Program.Player.HasBuff("LucianPassiveBuff") || Program.Player.IsDashing() ||
+                       Orbwalker.IsAutoAttacking || CustomEvents.PassiveUp))
+                    )
                 {
-
+                    if (Settings.UseQ && Program.Player.ManaPercent >= Settings.UseQmana &&
+                        enemy.IsValidTarget(SpellManager.Q.Range))
                     {
+                        SpellManager.Q.Cast(enemy);
+                    }
+
+                    if (Settings.UseQ1 && Program.Player.ManaPercent >= Settings.UseQ1mana &&
+                        enemy.IsValidTarget(SpellManager.Q1.Range))
+                    {
+                        var predPos = SpellManager.Q1.GetPrediction(enemy);
+                        var minions =
+                            EntityManager.MinionsAndMonsters.EnemyMinions.Where(
+                                mi => mi.Distance(Program.Player) <= Q.Range);
+                        var champs = EntityManager.Heroes.Enemies.Where(ch => ch.Distance(Program.Player) <= Q.Range);
+                        var monsters =
+                            EntityManager.MinionsAndMonsters.Monsters.Where(mo => mo.Distance(Program.Player) <= Q.Range);
                         {
+                            foreach (var minion in from minion in minions.OrderByDescending(x => x.Health)
+                                let polygon = new Geometry.Polygon.Rectangle(
+                                    (Vector2) Program.Player.ServerPosition,
+                                    Program.Player.ServerPosition.Extend(minion.ServerPosition, SpellManager.Q1.Range),
+                                    65f)
+                                where polygon.IsInside(predPos.CastPosition)
+                                select minion)
+                            {
+                                Q.Cast(minion);
+                            }
 
+                            foreach (var champ in from champ in champs
+                                let polygon = new Geometry.Polygon.Rectangle(
+                                    (Vector2) Program.Player.ServerPosition,
+                                    Program.Player.ServerPosition.Extend(champ.ServerPosition, SpellManager.Q1.Range),
+                                    65f)
+                                where polygon.IsInside(predPos.CastPosition)
+                                select champ)
+                            {
+                                Q.Cast(champ);
+                            }
 
+                            foreach (var monster in from monster in monsters
+                                let polygon = new Geometry.Polygon.Rectangle(
+                                    (Vector2) Program.Player.ServerPosition,
+                                    Program.Player.ServerPosition.Extend(monster.ServerPosition, SpellManager.Q1.Range),
+                                    65f)
+                                where polygon.IsInside(predPos.CastPosition)
+                                select monster)
+                            {
+                                Q.Cast(monster);
+                            }
                         }
                     }
                 }
 
+
+                if (SpellManager.W.IsReady() && Settings.UseW && Program.Player.ManaPercent >= Settings.UseWmana &&
+                    enemy.IsKillable(W.Range) && !Program.Player.Spellbook.IsCastingSpell &&
+                    !(Settings.SpellWeaving &&
+                      (Program.Player.HasBuff("LucianPassiveBuff") || Program.Player.IsDashing() ||
+                       Orbwalker.IsAutoAttacking || CustomEvents.PassiveUp))
+                    )
+                {
+                    var wPred = SpellManager.W.GetPrediction(enemy);
+                    if (wPred.HitChance >= HitChance.Medium)
+                        SpellManager.W.Cast(enemy.ServerPosition);
+                    if (wPred.HitChance != HitChance.Collision || !wPred.CollisionObjects.Any()) return;
+                    if (wPred.CollisionObjects.FirstOrDefault().Distance(enemy) <= 40)
+                        SpellManager.W.Cast(enemy.ServerPosition);
+                }
+
+
+                if (SpellManager.R.IsReady() && Settings.UseR && enemy.IsKillable(R.Range) &&
+                    (enemy.HasBuffOfType(BuffType.Snare) || enemy.HasBuffOfType(BuffType.Stun)) &&
+                    !Program.Player.HasBuff("LucianR") &&
+                    !(Settings.SpellWeaving &&
+                      Program.Player.IsDashing()))
+                {
+                    SpellManager.R.Cast(enemy);
+                }
             }
         }
     }
