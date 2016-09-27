1ab5733da27ede787a442d6c7ba0ba22f0261417
DamnedNooB <singed1306@gmail.com>
2016-09-27 18:39:02 +0200

    LazyIllaoi2.0
    :spaghetti:

---
+++ LazyIllaoi2.0/LazyIllaoi2.0/Modes/LastHit.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using EloBuddy;
+using EloBuddy.SDK;
+using Settings = LazyIllaoi2._0.Config.Modes.LastHit;
+
+namespace LazyIllaoi2._0.Modes
+{
+    public sealed class LastHit : ModeBase
+    {
+        public override bool ShouldBeExecuted()
+        {
+            return Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LastHit);
+        }
+
+        public override void Execute()
+        {
+        }
+    }
+}
---
+++ LazyIllaoi2.0/LazyIllaoi2.0/SpellManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+
+namespace LazyIllaoi2._0
+{
+    public static class SpellManager
+    {
+        static SpellManager()
+        {
+            Q = new Spell.Skillshot(SpellSlot.Q, 800, SkillShotType.Linear, 750, int.MaxValue, 100);
+            {
+                Q.AllowedCollisionCount = int.MaxValue;
+                Q.MinimumHitChance = HitChance.Medium;
+            }
+
+            W = new Spell.Active(SpellSlot.W, 400);
+
+            E = new Spell.Skillshot(SpellSlot.E, 900, SkillShotType.Linear, 250, 1900, 50);
+            {
+                E.AllowedCollisionCount = 0;
+                E.MinimumHitChance = HitChance.High;
+            }
+
+            R = new Spell.Active(SpellSlot.R, 450);
+        }
+
+        public static Spell.Skillshot Q { get; set; }
+        public static Spell.Active W { get; set; }
+        public static Spell.Skillshot E { get; set; }
+        public static Spell.Active R { get; set; }
+
+        public static void Initialize()
+        {
+        }
+    }
+}
